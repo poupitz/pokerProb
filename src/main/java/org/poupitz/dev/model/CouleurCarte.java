4930d3ef936197a4c29026715737925dc291ca1f
poupitz <vinc.portal@gmail.com>
2013-03-04 00:17:33 +0100

    Ajout des enums pour la valeur et la couleur des cartes.

---
+++ src/main/java/org/poupitz/dev/model/CouleurCarte.java
@@ -0,0 +1,9 @@
+package org.poupitz.dev.model;
+
+public enum CouleurCarte {
+	
+	PIQUE, COEUR, CARREAU, TREFLE;
+	
+	
+
+}
---
+++ src/main/java/org/poupitz/dev/model/ValeurCarte.java
@@ -0,0 +1,6 @@
+package org.poupitz.dev.model;
+
+public enum ValeurCarte {
+
+	AS, ROI, DAME, VALET, DIX, NEUF, HUIT, SEPT, SIX, CINQ, QUATRE, TROIS, DEUX;
+}
